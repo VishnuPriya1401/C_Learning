6abe117c1c867a3c4146e1ee5d624c7c19a13550
VishnuPriya1401 <145310382+VishnuPriya1401@users.noreply.github.com>
2024-11-19 15:13:32 +0530

    sum.c update

--- Practice-Exercise/sum.c
+++ Practice-Exercise/sum.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main()
+{
+    int a, b, sum=0;
+    printf("Enter two numners a and b:");
+    scanf("%d %d", &a,&b);
+    sum=a+b;
+    printf("Sum of two number: %d",sum);
+    return 0;
+}
